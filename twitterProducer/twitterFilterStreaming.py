ad58cbac4c1fa954804acdf90a05c27bab2834be
My Ho <mydiemho@gmail.com>
2015-06-20 21:23:51 -0700

    testing streaming script

--- twitterProducer/twitterFilterStreaming.py
+++ twitterProducer/twitterFilterStreaming.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 __author__ = 'myho'
 
 # https://realpython.com/blog/python/twitter-sentiment-python-docker-elasticsearch-kibana/
@@ -12,15 +14,16 @@ from twython import TwythonStreamer
 from config import *
 from clusterConfig import *
 
-
 # kafka setup
-producer = SimpleProducer(kafka)
-topicName = "twitterStream"
-
+kafka_client = KafkaClient(kafka)
+producer = SimpleProducer(kafka_client)
+topicName = "twitterFilterStream"
 
 class TweetStreamListener(TwythonStreamer):
     def on_success(self, data):
-        if 'text' in data:
+        # look for 'text' to filter ill-formatted tweets in stream
+        if 'text' in data and data['coordinates'] != None:
+            print data['coordinates']
             producer.send_messages(topicName, json.dumps(data))
 
     def on_error(self, status_code, data):
@@ -32,3 +35,6 @@ class TweetStreamListener(TwythonStreamer):
 
 if __name__ == '__main__':
     stream = TweetStreamListener(consumer_key, consumer_secret, access_key, access_secret)
+
+    # only include tweets in sf
+    stream.statuses.filter(locations='-122.75,36.8,-121.75,37.8')
---
+++ twitterProducer/twitterSampleStreaming.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+__author__ = 'myho'
+
+# https://realpython.com/blog/python/twitter-sentiment-python-docker-elasticsearch-kibana/
+# https://github.com/shafiab/HashtagCashtag/blob/master/Kafka/twitterProducer/twitterAPI.py
+
+import json
+
+from kafka import *
+
+from twython import TwythonStreamer
+
+from config import *
+from clusterConfig import *
+
+
+# kafka setup
+kafka_client = KafkaClient(kafka)
+producer = SimpleProducer(kafka_client)
+topicName = "twitterSampleStream"
+
+class TweetStreamListener(TwythonStreamer):
+    def on_success(self, data):
+        # look for 'text' to filter ill-formatted tweets in stream
+        if 'text' in data and data['coordinates'] != None:
+            print data['coordinates']
+        # producer.send_messages(topicName, json.dumps(data))
+
+    def on_error(self, status_code, data):
+        print '!!! error occurred !!!'
+        print self
+        print data
+        print status_code
+
+
+if __name__ == '__main__':
+    stream = TweetStreamListener(consumer_key, consumer_secret, access_key, access_secret)
+    stream.statuses.sample()
